7b43bd1f6ae3ca8b8fe04d7e67adc2da86239b60
Edu_Santos2566 <eduardocorral296@gmail.com>
2024-08-28 20:34:26 -0300

    Update: Connect the Course list in API

--- routes/faculdades/faculdades.py
+++ routes/faculdades/faculdades.py
@@ -10,8 +10,8 @@ def faculdades_menu():
 @faculdades_blueprint.route('/faculty-information/<int:faculty_id>')
 def faculty_information(faculty_id):
     # Faz a requisição para obter os dados da faculdade com o ID fornecido
-    response = requests.get("https://c3e1bccd-e40f-4642-a5e0-6b45d108af4b-00-3g9s82kxsdncm.picard.replit.dev/")
-    faculty_data = response.json()  # Recebe os dados da API
+    response_faculty = requests.get("https://c3e1bccd-e40f-4642-a5e0-6b45d108af4b-00-3g9s82kxsdncm.picard.replit.dev/")
+    faculty_data = response_faculty.json()  # Recebe os dados da API
 
     # Filtra a faculdade específica com base no ID
     faculty = next((f for f in faculty_data if f["id_faculdade"] == faculty_id), None)
@@ -19,6 +19,14 @@ def faculty_information(faculty_id):
     if faculty is None:
         return "Faculdade não encontrada", 404
 
+    response_courses = requests.get("https://c3e1bccd-e40f-4642-a5e0-6b45d108af4b-00-3g9s82kxsdncm.picard.replit.dev/courses")
+    courses_data = response_courses.json()
+
+    cursos = [course['nome_curso'] for course in courses_data if course['id_faculdade'] == faculty_id]
+    
+    # Adiciona a lista de cursos ao dicionário da faculdade
+    faculty['cursos'] = cursos
+    
     # Renderiza o template passando os dados da faculdade específica
     return render_template('faculty-information.html', faculty=faculty)
 
