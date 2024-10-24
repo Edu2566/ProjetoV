4af9d0a61f73cb38357b31873e3fcdc5ae595d3e
Edu_Santos2566 <eduardocorral296@gmail.com>
2024-10-24 15:11:14 -0300

    Update: ENEM Conditional

--- api/CollegeAPI.py
+++ api/CollegeAPI.py
@@ -15,7 +15,6 @@ class CollegeAPI:
             return None
 
     def get_college_by_id(self, college_id):
-        # Fetch data from all relevant endpoints
         college_data = self.fetch_data('')
         courses_data = self.fetch_data('courses')
         exams_data = self.fetch_data('exams')
@@ -28,13 +27,11 @@ class CollegeAPI:
         information_data is None or location_data is None or template_data is None or date_data is None):
             return {'error': 'Failed to fetch data from API'}
 
-        # Find the college information
         college = next((item for item in college_data if item['college_id'] == college_id), None)
 
         if not college:
             return {'error': 'College not found'}
 
-        # Group exams by year
         exams_by_year = {}
         for exam in exams_data:
             year = exam.get('exam_year')
@@ -42,7 +39,6 @@ class CollegeAPI:
                 exams_by_year[year] = []
             exams_by_year[year].append(exam)
 
-        # Create a combined dictionary with all related information
         result = {
             'college': college,
             'courses': [item for item in courses_data if item['college_id'] == college_id],
@@ -56,7 +52,6 @@ class CollegeAPI:
         return result
 
     def get_all_colleges(self):
-        # Fetch data from the main endpoint
         college_data = self.fetch_data('')
 
         if college_data is None:
@@ -81,13 +76,11 @@ class CollegeAPI:
         return course_image_data
 
     def get_exam_by_id(self, exam_id):
-        # Fetch data from the exams endpoint
         exams_data = self.fetch_data('exams')
 
         if exams_data is None:
             return {'error': 'Failed to fetch data from API'}
 
-        # Find the specific exam information
         exam = next((item for item in exams_data if item['exam_id'] == exam_id), None)
 
         if not exam:
@@ -97,50 +90,42 @@ class CollegeAPI:
     
     def get_course_by_id(self, course_id):
         """Busca um curso específico pelo course_id e inclui a imagem correspondente."""
-        # Busca todos os cursos
         course_data = self.fetch_data('courses')
         course_image_data = self.fetch_data('course-images')
 
         if course_data is None or course_image_data is None:
             return {'error': 'Failed to fetch course or image data from API'}
 
-        # Busca o curso específico pelo course_id
         course = next((item for item in course_data if item['course_id'] == course_id), None)
 
         if not course:
             return {'error': 'Course not found'}
 
-        # Busca a imagem correspondente ao tipo de ciência do curso
         course_image = next((image for image in course_image_data if image['science_type'] == course['science_type']), None)
 
         if course_image:
             course['course_image_url'] = course_image['course_image_url']
         else:
-            course['course_image_url'] = None  # Caso não tenha uma imagem correspondente
+            course['course_image_url'] = None
 
         return course
     
     
     def college_for_courses(self, course_id):
-        # Busca todos os cursos
         courses_data = self.fetch_data('courses')
         
         if courses_data is None:
             return {'error': 'Failed to fetch courses data from API'}
         
-        # Encontra todos os college_id para o course_id específico
         related_college_ids = {course['college_id'] for course in courses_data if course['course_id'] == course_id}
 
         if not related_college_ids:
             return {'error': 'No related colleges found for this course'}
 
-        # Busca todas as faculdades
-        colleges_data = self.fetch_data('')  # Aqui estamos assumindo que a função fetch_data() retornará todas as faculdades
-
+        colleges_data = self.fetch_data('')
         if colleges_data is None:
             return {'error': 'Failed to fetch colleges data from API'}
 
-        # Filtra as faculdades que possuem os college_id encontrados
         related_colleges = [college for college in colleges_data if college['college_id'] in related_college_ids]
 
         return related_colleges
